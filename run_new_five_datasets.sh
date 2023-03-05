eadbe79a1ec671f742ec79b0b4fd855aae5dc354
Jason Ding <zhoujie.ding@berkeley.edu>
2023-03-05 02:12:05 -0800

    add new dataset script

--- run_new_five_datasets.sh
+++ run_new_five_datasets.sh
@@ -2,5 +2,5 @@ project="new_five_datasets"
 
 python -u main.py --dataset ${project} --input_dir \
 /data3/dlvp_local_data/dataset_merged/new_five_datasets/ \
---feature_size 225 --model_type ggnn --train \
+--feature_size 224 --model_type ggnn --batch_size 128 --train \
 2>&1 | tee logs/${project}_$(date "+%m.%d-%H.%M.%S").log
